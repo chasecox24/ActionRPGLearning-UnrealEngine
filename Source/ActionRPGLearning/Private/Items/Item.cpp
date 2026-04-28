0529780af89ceb1abbbf399241b027f79fab3c58
Chase Cox <chasecox24@gmail.com>
2026-04-27 19:38:27 -0500

    Lesson 46 Completed: GEngine->AddOnScreenDebugMessage(key, floatDuration, FColor::, FString("Message"));

--- Source/ActionRPGLearning/Private/Items/Item.cpp
+++ Source/ActionRPGLearning/Private/Items/Item.cpp
@@ -15,6 +15,11 @@ void AItem::BeginPlay()
 	Super::BeginPlay(); //calling the parent class's BeginPlay// the parent version might have functionality that we want to keep, so we call it first before adding our own functionality
 	
 	UE_LOG(LogTemp, Warning, TEXT("Begin play called!"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("ON SCREEN MESSAGE!!!"));
+	}
 }
 
 
