d45c3ba99707887141858253da98053bafa3f675
Chase Cox <chasecox24@gmail.com>
2026-05-06 18:23:20 -0500

    Lesson 62 done. UPROPERTY(EditAnywhere, BlueprintReadOnly): only allows to get (getter) the variable UPROPERTY(EditAnywhere, BlueprintReadWrite): allows (get/set) the variable
    To see the C++ variables, you have to click the GEAR icon, then click Show Inherited Variables below components in the My Blueprint area.
    
    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters"): gives it a category inside the details panel and blueprint.
    
    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true")): able to expose a private variable to blueprint using the meta specificers, can only read

--- Intermediate/Build/Win64/UnrealEditor/Inc/ActionRPGLearning/UHT/Item.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/ActionRPGLearning/UHT/Item.gen.cpp
@@ -33,16 +33,22 @@ struct Z_Construct_UClass_AItem_Statics
 		{ "ModuleRelativePath", "Public/Items/Item.h" },
 	};
 	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_Amplitude_MetaData[] = {
-		{ "Category", "Item" },
+		{ "Category", "Sine Parameters" },
 		{ "ModuleRelativePath", "Public/Items/Item.h" },
 	};
 	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_TimeConstant_MetaData[] = {
+		{ "Category", "Sine Parameters" },
+		{ "ModuleRelativePath", "Public/Items/Item.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_RunningTime_MetaData[] = {
+		{ "AllowPrivateAccess", "true" },
 		{ "Category", "Item" },
 		{ "ModuleRelativePath", "Public/Items/Item.h" },
 	};
 #endif // WITH_METADATA
 	static const UECodeGen_Private::FFloatPropertyParams NewProp_Amplitude;
 	static const UECodeGen_Private::FFloatPropertyParams NewProp_TimeConstant;
+	static const UECodeGen_Private::FFloatPropertyParams NewProp_RunningTime;
 	static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
 	static UObject* (*const DependentSingletons[])();
 	static constexpr FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
@@ -50,11 +56,13 @@ struct Z_Construct_UClass_AItem_Statics
 	};
 	static const UECodeGen_Private::FClassParams ClassParams;
 };
-const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_AItem_Statics::NewProp_Amplitude = { "Amplitude", nullptr, (EPropertyFlags)0x0040000000000001, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AItem, Amplitude), METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_Amplitude_MetaData), NewProp_Amplitude_MetaData) };
-const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_AItem_Statics::NewProp_TimeConstant = { "TimeConstant", nullptr, (EPropertyFlags)0x0040000000000001, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AItem, TimeConstant), METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_TimeConstant_MetaData), NewProp_TimeConstant_MetaData) };
+const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_AItem_Statics::NewProp_Amplitude = { "Amplitude", nullptr, (EPropertyFlags)0x0020080000000005, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AItem, Amplitude), METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_Amplitude_MetaData), NewProp_Amplitude_MetaData) };
+const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_AItem_Statics::NewProp_TimeConstant = { "TimeConstant", nullptr, (EPropertyFlags)0x0020080000000005, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AItem, TimeConstant), METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_TimeConstant_MetaData), NewProp_TimeConstant_MetaData) };
+const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_AItem_Statics::NewProp_RunningTime = { "RunningTime", nullptr, (EPropertyFlags)0x0040000000020015, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AItem, RunningTime), METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_RunningTime_MetaData), NewProp_RunningTime_MetaData) };
 const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_AItem_Statics::PropPointers[] = {
 	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AItem_Statics::NewProp_Amplitude,
 	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AItem_Statics::NewProp_TimeConstant,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AItem_Statics::NewProp_RunningTime,
 };
 static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_AItem_Statics::PropPointers) < 2048);
 UObject* (*const Z_Construct_UClass_AItem_Statics::DependentSingletons[])() = {
@@ -97,10 +105,10 @@ AItem::~AItem() {}
 struct Z_CompiledInDeferFile_FID_Users_chase_Desktop_Unreal_Projects_ActionRPGLearning_Source_ActionRPGLearning_Public_Items_Item_h_Statics
 {
 	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
-		{ Z_Construct_UClass_AItem, AItem::StaticClass, TEXT("AItem"), &Z_Registration_Info_UClass_AItem, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AItem), 3059437598U) },
+		{ Z_Construct_UClass_AItem, AItem::StaticClass, TEXT("AItem"), &Z_Registration_Info_UClass_AItem, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AItem), 3343804144U) },
 	};
 };
-static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_chase_Desktop_Unreal_Projects_ActionRPGLearning_Source_ActionRPGLearning_Public_Items_Item_h_2587391420(TEXT("/Script/ActionRPGLearning"),
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_chase_Desktop_Unreal_Projects_ActionRPGLearning_Source_ActionRPGLearning_Public_Items_Item_h_4278077364(TEXT("/Script/ActionRPGLearning"),
 	Z_CompiledInDeferFile_FID_Users_chase_Desktop_Unreal_Projects_ActionRPGLearning_Source_ActionRPGLearning_Public_Items_Item_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_chase_Desktop_Unreal_Projects_ActionRPGLearning_Source_ActionRPGLearning_Public_Items_Item_h_Statics::ClassInfo),
 	nullptr, 0,
 	nullptr, 0);
